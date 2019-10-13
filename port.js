8fbbaf13b130b12e7eed11e3c30b2e1a8d2257d4
ModernFarmer <47311260+ModernFarmer@users.noreply.github.com>
2019-10-13 22:55:07 +0800

    Add files via upload

---
+++ port.js
@@ -0,0 +1,39 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+const multiparty=require('multiparty');
+const async4mysql=require('async4mysql');
+
+const app=express();
+const port=8888;
+app.listen(port, function(){
+	console.log('---------------- > node服务已开启, 接口: '+port+' !');
+});
+
+app.all('*',function (req, res, next) {                   //使服务可被跨域请求
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    if (req.method == 'OPTIONS') {
+        res.send(200); /让options请求快速返回/
+    }
+    else {
+        next();
+    };
+});
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+const db=new async4mysql({
+	host:'localhost',
+	user:'root',
+	port:3306,
+	password:'123456',
+	database:'demo'
+});
+
+app.get('/demo', async (req, res)=>{
+	
+});
+app.post('/demo', async (req, res)=>{
+	
+});
