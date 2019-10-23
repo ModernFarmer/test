35e95a2e46c52e79155090a59408138042e2a527
ModernFarmer <47311260+ModernFarmer@users.noreply.github.com>
2019-10-23 20:03:51 +0800

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -1,9 +1,7 @@
-/*  */
-
 class ___UM_ajax{
 	constructor(){
 		this.config={  // ajax的全局配置
-			baseUrl:'',
+			baseUrl:'http://localhost:8888',
 			timeout:null,
 			ontimeout:null,  // [function]  自带固定参数: XMLHttpRequest对象
 			responseType:'',
@@ -11,59 +9,83 @@ class ___UM_ajax{
 			onprogress:null  // [function]  自带固定参数: ProgressEvent对象, 它的.loaded属性代表已经完成发送部分的文件大小[number], 它的.total属性代表整个文件大小[number]
 		};
 
-		this.beforeRequest=function(config){  // [function]  自带固定参数: config对象, 代表ajax的*全局*配置
+		this.temporary={};  // 存放单次临时数据的对象
+
+		this.beforeRequest=function(config){  // 请求发送前触发 [function]  默认null  自带固定参数: config对象; 代表ajax的*全局*配置
+			// 如果beforeRequest里面有异步函数, 则必须返回一个Promise
+			// 比如:
+			/*return new Promise((resolve, reject)=>{
+				setTimeout(()=>{
+					// todo ...
+					resolve(config);
+				}, 2000);
+			});*/
 			// todo ...
-			return config;
+			return config;  // 必须返回config
 		};
 
-		this.beforeRequest=null;  // [null/function]
-		this.responsed=null; // [null/function]
+		this.responsed=function(result, XMLObj){  // 接收请求数据后立即触发 [function]  默认null  自带固定参数: 1:接收到的数据, 2:XMLHttpRequest对象; 代表ajax的*全局*配置
+			// todo
+		};
 
-
+		// this.beforeRequest=null;  // [null/function]
+		// this.responsed=null; // [null/function]
 	}
-	async get(url, json){
-        let trueConfig=await this.__before(this.beforeRequest, this.config);
-        if(!trueConfig)trueConfig=this.config;
-		let dataStr=await __toBeEncoded(json);
-		let ajaxObj=null;
-        if(window.XMLHttpRequest){
-            ajaxObj=new XMLHttpRequest();
-        }else{
-            ajaxObj=new ActiveXObject('Microsoft.XMLHTTP');
-        };
+	get(url, json){
+		return new Promise(async (resolve, reject)=>{
+			let trueConfig=null;
+	        if(this.beforeRequest)trueConfig=await this.__before(this.beforeRequest, this.config);
+	        if(!trueConfig)trueConfig=this.config;
+	        let dataObj=null;
+			if(json)dataObj=this.__toBeEncoded(json);
+			let ajaxObj=null;
+	        if(window.XMLHttpRequest){
+	            ajaxObj=new XMLHttpRequest();
+	        }else{
+	            ajaxObj=new ActiveXObject('Microsoft.XMLHTTP');
+	        };
 
-        ajaxObj.open('GET',trueConfig.baseUrl+url+'?'+dataStr, true);
+	        let trueUrl=dataObj?trueConfig.baseUrl+url+'?'+dataObj.encoded:trueConfig.baseUrl+url;
 
-        if(trueConfig.responseType)ajaxObj.responseType=trueConfig.responseType;
+	        ajaxObj.open('GET',trueUrl, true);
 
-        if(trueConfig.headers){
-        	for(let name in trueConfig.headers){
-        		ajaxObj.setRequestHeader(name, trueConfig.headers[name]);
-        	};
-        }
+	        if(trueConfig.responseType)ajaxObj.responseType=trueConfig.responseType;
 
-        if(trueConfig.onprogress && typeof trueConfig.onprogress==='function'){
-        	ajaxObj.onprogress=function(eventObj){
-        		trueConfig.onprogress(eventObj);
-        	};
-        }
+	        if(trueConfig.headers){
+	        	for(let name in trueConfig.headers){
+	        		ajaxObj.setRequestHeader(name, trueConfig.headers[name]);
+	        	};
+	        }
 
-        if(trueConfig.timeout){
-        	ajaxObj.timeout=trueConfig.timeout;
-        	if(trueConfig.ontimeout && typeof trueConfig.ontimeout==='function'){
-        		ajaxObj.ontimeout=function(ajaxObj){
-        			trueConfig.ontimeout(ajaxObj);
-        		};
-        	}
-        }
+	        if(trueConfig.onprogress && typeof trueConfig.onprogress==='function'){
+	        	ajaxObj.onprogress=function(eventObj){
+	        		trueConfig.onprogress(eventObj);
+	        	};
+	        }
 
-        ajaxObj.send();
+	        if(trueConfig.timeout){
+	        	ajaxObj.timeout=trueConfig.timeout;
+	        	if(trueConfig.ontimeout && typeof trueConfig.ontimeout==='function'){
+	        		ajaxObj.ontimeout=function(ajaxObj){
+	        			trueConfig.ontimeout(ajaxObj);
+	        		};
+	        	}
+	        }
 
-        ajaxObj.onreadystatechange=()=>{
-            if(ajaxObj.readyState==4){
-                
-            }
-        };
+	        ajaxObj.send();
+
+	        ajaxObj.onreadystatechange=async ()=>{
+	            if(ajaxObj.readyState==4){
+	            	let data=ajaxObj.response;
+	            	if(this.responsed)this.__after(this.responsed, data, ajaxObj);
+            		if(typeof data==='string'){
+            			resolve(JSON.parse(data));
+            		}else{
+            			resolve(data);
+            		};
+	            }
+	        };
+		});
 	}
 	post(){
 
@@ -85,47 +107,43 @@ class ___UM_ajax{
 	}
 	__toBeEncoded(item){
 		if(typeof item=='string'){
-			return new Promise((resolve, reject)=>{
-				resolve({
-					header:'application/json',
-					encoded:JSON.stringify(item)
-				});
-			});
+			return {
+				header:'application/json',
+				encoded:JSON.stringify(item)
+			};
 		}else{
 		    let arr=[];
 		    for(let name in item){
 		        arr.push(name+'='+item[name]);
 		    };
-		    return new Promise((resolve, reject)=>{
-		    	resolve({
-					header:'application/x-www-form-urlencoded',
-					encoded:arr.join('&')
-			    });
-		    });
+	    	return {
+				header:'application/x-www-form-urlencoded',
+				encoded:arr.join('&')
+		    };
 		};
 	}
 	__before(fn, config){
-		if(!fn){
-			return new Promise((resolve, reject)=>{
+		return new Promise((resolve, reject)=>{
+			if(!fn){
 				resolve(null);
-			});
-		}else{
-			return new Promise((resolve, reject)=>{
+			}else{
 				resolve(fn(Object.assign({}, config)));
-			});
-		};
+			};
+		});
 	}
-	__next(ajaxObj, json){
-        if(ajaxObj.status>=200 && ajaxObj.status<300 || ajaxObj.status==304){
-            json.success && json.success(JSON.parse(ajaxObj.responseText));
-        }else{
-            json.error && json.error(ajaxObj.status);
-        };
+	__after(fn, result, XMLObj){
+		if(fn && typeof fn==='function'){
+			if(typeof result==='string'){
+				fn(JSON.parse(result), XMLObj);
+			}else{
+				fn(result, XMLObj);
+			};
+		};
 	}
 }
 
 let ajax=new ___UM_ajax();
 
-ajax.json='abc';
-
-console.log(ajax)
+ajax.get('/demo').then(data=>{
+	console.log(data);
+})
